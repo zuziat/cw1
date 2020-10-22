df5b6cd6f701bb022de81900e51494e1e9dcd2be
Zuzanna Trochimiak <s18919@pjwstk.edu.pl>
2020-10-22 14:09:29 +0200

    "dodane dodatkowe wymagania"

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Net.Http;
+using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace cw1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            var httpClient = new HttpClient();
+                try
+                {
+                
+                    var response = await httpClient.GetAsync(args[0]);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var html = await response.Content.ReadAsStringAsync();
+                        var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                        var matches = regex.Matches(html);
+                        var sum = 0;
+                        foreach (var i in matches)
+                        {
+                            Console.WriteLine(i);
+                            sum++;
+                        }
+                        if (sum == 0)
+                        {
+                            Console.WriteLine("Nie znaleziono adresów email");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Błąd w czasie pobierania strony");
+                    }
+
+                } catch (ArgumentNullException) {
+
+
+                }
+
+            httpClient.Dispose();
+
+            }    
+                   
     }
 }
